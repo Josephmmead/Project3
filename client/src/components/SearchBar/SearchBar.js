5e2f7a30a5685fd3332e2288a9edd4ef455b2405
Joseph Mead <josephmmead@gmail.com>
2021-01-23 14:09:56 -0500

    components, pages, app.js, seeds

---
+++ client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '.SearchBar.css'
+export const SearchBar = ({placeholder, handleChange}) => (
+    <input
+    className= 'search'
+    type= 'search'
+    placeholder= {placeholder}
+    onChange= {handleChange}/>
+)
