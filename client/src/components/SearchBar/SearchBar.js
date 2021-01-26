260854ce3b243b0ca5629c25e3340313b274454c
Joseph Mead <josephmmead@gmail.com>
2021-01-25 22:41:15 -0500

    fixed searchbar.css import

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './SearchBar.css'
 
-    function SearchBar ({placeholder, handleChange}) {
-        return(
-            <input
-                className= 'search'
-                type= 'search'
-                placeholder= "Search"
-                // onChange= {handleChange}
-                />
-        )
-    } 
+
+function SearchBar({placeholder, handleChange}) {
+    return(
+        <input
+        className= 'search'
+        type= 'search'
+        placeholder= {placeholder}
+        onChange= {handleChange}/>
+    )
     
+};
+
 export default SearchBar;
