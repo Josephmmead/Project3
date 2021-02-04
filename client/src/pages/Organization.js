739c3ebe700eecdf16637c6666f56f9d01d7eec9
Joseph Mead <josephmmead@gmail.com>
2021-02-03 21:27:18 -0500

    added features on home page

--- client/src/pages/Organization.js
+++ client/src/pages/Organization.js
@@ -2,10 +2,8 @@ import React, { useState, useEffect,  } from 'react';
 import { Container, Row, Col} from "../components/Grid/Grid";
 import API from "../utils/API";
 import { useParams } from "react-router-dom";
-import './css/Organization.css'
-
-
-
+import './css/Organization.css';
+import { Card, Button } from 'react-bootstrap'
 
 
   function Organization()  {
@@ -14,60 +12,58 @@ import './css/Organization.css'
     const { id } = useParams()
 
     useEffect(() => {
- 
         API.getCharityById(id)
             .then(res => setCharity(res.data))
             .catch(err => console.log(err))
-            
     }, []);
-
-
 
         return(
         <div className="orgPage">
             <Container fluid>
-                <Row fluid>  
-                    <Col  size="md-12" >                       
-                            {Charity.map(data => (
-                                <div key={data._id}>  
-                                
-                                        <Col size="md-auto">
-                                            <div >
-                                                <img  className="img-fluid " src={data.thumbnail} alt=""></img> 
-                                            </div>
-                                        </Col>
-                                        <Col size="md-auto">
-                                            <h3 id="charityName">{data.name} </h3>
-                                            <h5>{data.mainCause}</h5>                                                                     
-                                            <p id="charityMission"> {data.mission}</p>
-                                        </Col>
-                                        <Col size= "md-auto">
-                                            <p> {data.address}</p>
-                                            <p> {data.city}, {data.state}</p>
-                                            <p> {data.phone}</p>
-                                            <p> {data.contactEmail}</p>
-                                            <p> EIN#: {data.EIN}</p>
-                                            <a href={data.href}> {data.href}</a>
-                                        <Col size= "md-auto">
-                                            <p>{data.donationMethod}</p>
-                                        </Col>
-                                        <Col size="md-auto">
-                                            <ul>
+                <Row className="row">
+                    <Col size="md-12 sm-12">
+                        {Charity.map(data => (
+                        <div key={data._id}>
+                            <Row >
+                                <Col size="md-1"></Col>
+                                <Col size="md-3">
+                                    <Card>
+                                        <Card.Img variant="top" src={data.thumbnail} alt="" />
+                                        <Card.Body>
+                                            <Card.Text>
+                                                <p>{data.address}</p>
+                                                <p> {data.city}, {data.state}</p>
+                                                <p> {data.phone}</p>
+                                                <p> {data.contactEmail}</p>
+                                                <p> EIN#: {data.EIN}</p>
+                                            </Card.Text>
+                                            <Button variant="primary" href={data.href}>Visit {data.name}</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>    
+                                <Col size="md-1"></Col>
+                                <Col size="md-6">
+                                    <h1 id="charityName">{data.name} </h1>
+                                    <h3>{data.mainCause}</h3>                                                                     
+                                    <p id="charityMission"> {data.mission}</p>
+                                    <h3>How to Donate</h3>
+                                    <p>{data.donationMethod}</p>
+                                    <h3>Currently Accepting the Following:</h3>
+                                    <Row >
+                                        <ul>
                                             {data.acceptedItems.map(data => (
-                                                <li>{data}</li>
+                                            <li>{data}</li>
                                             ))}
-                                            </ul>
-                                        </Col>
-                                        </Col>
-                                </div>
-                            ))}
-                        
-                        </Col>                   
+                                        </ul>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </div>
+                    ))}            
+                    </Col>
                 </Row>
-                
             </Container>
         </div>                
         )
     }
-
-export default Organization; 
+export default Organization;
